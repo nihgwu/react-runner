d979bd5b6323311aeaade37f0dad3c69644e2527
Neo Nie <nihgwu@live.com>
2019-06-27 22:39:57 +0800

    website: add ga

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -4,6 +4,12 @@ module.exports = {
     title: 'Website for react-runner',
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: 'UA-142848497-1',
+      },
+    },
     'gatsby-plugin-react-helmet',
     'gatsby-plugin-styled-components',
     'gatsby-plugin-offline',

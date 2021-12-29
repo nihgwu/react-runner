c66b2efda379037bbd9f8226c0bd4dea9e03a5cf
Neo Nie <nihgwu@live.com>
2021-12-29 00:53:17 +0000

    Updates

--- _next/static/FYwVQc_mBvUf3i5j3_VJN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/411-7e45df651339d78d.js","static/chunks/pages/index-3cdcebad5906fafa.js"],"/_error":["static/chunks/pages/_error-9734db4c9fd59614.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

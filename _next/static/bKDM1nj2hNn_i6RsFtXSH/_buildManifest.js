2b3ab7a9fb899580fab7068bee3959f5e99cff18
nihgwu <nihgwu@users.noreply.github.com>
2021-12-29 22:29:53 +0000

    deploy: 3f79b103964f916022c8e339d6555b8f6d557f2f

---
+++ _next/static/bKDM1nj2hNn_i6RsFtXSH/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/833-6ff0f1d66e19cc81.js","static/chunks/pages/index-889eed8d30e13b27.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

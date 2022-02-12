008ba30436e45a5fc648d9c4373bb402c7a662c5
Neo Nie <nihgwu@live.com>
2022-02-12 15:55:14 +0000

    chore: improve normalizeCode

--- packages/react-runner/src/transform.ts
+++ packages/react-runner/src/transform.ts
@@ -6,3 +6,10 @@ export const transform = (code: string) => {
     production: true,
   }).code.substring(13) // remove leading `"use strict";`
 }
+
+const firstStatementRegexp =
+  /^(\s*)(<[^>]*>|function[\(\s]|\(\)[\s=]|class\s)(.*)/
+
+export const normalizeCode = (code: string) => {
+  return code.replace(firstStatementRegexp, '$1export default $2$3')
+}

f3bc9315865d8dd592f38ecdebaf26e7d643de1a
Neo Nie <nihgwu@live.com>
2022-03-31 22:32:10 +0100

    chore(examples): add remix example

---
+++ examples/with-remix/app/routes/index.tsx
@@ -0,0 +1,56 @@
+import {
+  LiveProvider,
+  LiveEditor,
+  LivePreview,
+  LiveError,
+} from 'react-live-runner'
+
+const example = `
+export default function Demo() {
+  const [count, setCount] = React.useState(0)
+
+  return (
+    <button type="button" onClick={() => setCount((count) => count + 1)}>
+      count is: {count}
+    </button>
+  )
+}
+`.trim()
+
+export default function Index() {
+  return (
+    <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
+      <h1>Welcome to Remix</h1>
+      <LiveProvider code={example}>
+        <LiveEditor />
+        <LivePreview />
+        <LiveError />
+      </LiveProvider>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/blog"
+            rel="noreferrer"
+          >
+            15m Quickstart Blog Tutorial
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            href="https://remix.run/tutorials/jokes"
+            rel="noreferrer"
+          >
+            Deep Dive Jokes App Tutorial
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
+            Remix Docs
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}

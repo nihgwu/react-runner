4302228087520a5ba18d841cfdcba4eeda17bb9c
Neo Nie <nihgwu@live.com>
2022-03-31 22:29:43 +0100

    chore(examples): no more workaround

--- examples/with-vite/vite.config.ts
+++ examples/with-vite/vite.config.ts
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      // vite 2.8 will throw error `TypeError: import_NameManager.default is not a constructor`
-      // as it will try to import `.js` file instead of `.mjs` from `index.mjs`
-      sucrase: 'sucrase/dist/index.js',
-    },
-  },
   plugins: [react()],
+  define: {
+    global: 'globalThis',
+  },
 })
